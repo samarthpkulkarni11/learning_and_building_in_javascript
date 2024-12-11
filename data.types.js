6519465e6d58548a6c2d28b96e1974895becabf1
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-12 00:20:47 +0530

    Learning and Working around data types in JS

---
+++ data.types.js
@@ -0,0 +1,9 @@
+//Covering different data types in JS
+
+`There are majorly two different data types in JS they are -
+
+Primitive Data types - These are the one which are actually predefined and has an actual meaning to the data stored under a particular type.
+Examples - Number, Null, String, Symbol, Boolean, BigInt and Undefined.
+
+Non Primitve Data types - These are the one which are user defined and does not have any meaning to their names and can store any format of data.
+Examples - Arrays, Objects, Regular Expressions, Functions, Maps, Sets etc `

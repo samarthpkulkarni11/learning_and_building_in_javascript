70b73ed910f613e148fcfc99129ecf294b6caa84
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-12 00:01:21 +0530

    Adding up a little brief history of JS

--- javascript.story.js
+++ javascript.story.js
@@ -1,1 +1,14 @@
-//Some info on javascript to be known - Just read through for few minutes.
+`Some info on javascript to be known - Just read through for few minutes.
+
+ Earlier in 90's there use to be the browser known as Netscape which was very very popular and had a massive 90% market share back. The founder Mark Andressen had
+ thought of an idea of coming up with dynamic websites which led him to get a programming language that can run on browsers. So he had assigned Branden Erich as the person
+ to develop programming language that can run on browsers.
+
+Erich had written the javascript as the language in just 10 days and fun fact is the original name or first name of javascript is Mocha later changed to liveScript and later
+changed to javascript as the java was trending everywhere back in times and javascript has like zero relation with java
+To avoid any case of disperencies or different browsers developing their own language for their browsers (as microsoft had did for their browser by developing Jscript), ECMA
+which stands for European Computers Market Association came up with laying strict rules around how the javascript as a language will be used for web browsers and later the same
+group kept enhancing the language by releasing different versions like ES4, ES5 and ES6. This is the brief history of Mocha aka LiveScript aka JavaScript!
+One more most popular and well known fact that Javascript earlier used to be a interpreted language (converting the code into byte executable code line by line and then executing)
+ but by the graceful powerful introductions of chromes engines like V8 did boost the javascript and now Javascript is popularly known as Just-In-Time Compiler language which 
+compiles the code as it is about to run on a system or a machine.`

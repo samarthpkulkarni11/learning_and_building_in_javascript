9277b97b74f706d7f4aa665a8290df1c533220b5
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-14 23:46:52 +0530

    Adding up the alert, prompt and confirm to the JS flow

---
+++ alert.prompt.confirm.js
@@ -0,0 +1,9 @@
+// We will cover and check about alert, prompt and confirm here
+
+//General implementation for alert, confirm and prompt.
+
+const alertUser = alert("Do you wanna currently view this web page?");
+
+const reconfirmUser = confirm("Are you sure about it?");
+
+const promptUser = prompt("How are you feeling about JS?");

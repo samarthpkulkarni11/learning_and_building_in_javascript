dc0132ed0946dbf32f7a17a85fcbeba8e0ef3675
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-26 00:02:16 +0530

    feat(js-basics): Pushing Math properties to the flow

--- mathobject.js
+++ mathobject.js
@@ -2,6 +2,17 @@
 //Math object in console
 //math.floor(), .ceil(), .pow(), .sqrt(), .random(), .round().
 
+//properties for Math.Object()
+E: 2.718281828459045
+LN2: 0.6931471805599453
+LN10: 2.302585092994046
+LOG2E: 1.4426950408889634
+LOG10E: 0.4342944819032518
+PI: 3.141592653589793
+SQRT1_2: 0.7071067811865476
+SQRT2: 1.4142135623730951
+
+//methods for Math.Object()
 let valueForMathObject = 12.54;
 console.log(Math.floor(valueForMathObject)); //gives up 12 as it just deletes the decimal value from it and returns back the whole.
 console.log(Math.ceil(valueForMathObject)); //gives 13 as it rounds up to next big value

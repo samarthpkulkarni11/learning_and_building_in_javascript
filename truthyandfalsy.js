25d237f07cbe2f8e0b23fdc5f3ac927ceaae8720
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-26 01:39:13 +0530

    feat(js-basics): Adding truthy and falsy values to the flow

---
+++ truthyandfalsy.js
@@ -0,0 +1,13 @@
+//We are going to discuss about truthy and falsy values in JS
+
+//There are infinite truthy values in JS so let us focus on what all falsy values are there in JS
+
+//These are the following falsy values in JS they are -
+
+`0` - returns false when checked through Boolean(0) {fot both 0 and - 0 }
+`NaN` - a Not an Number returns false
+`''` - an empty string also returns false
+    `undefined` - returns false
+    `null` - also returns false 
+        
+//These are the 5 falsy values under Javascript which returns false as the result!!

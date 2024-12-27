8aa15fc5bf79d995e5df9ad006f8082b3beac9df
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-12-28 00:39:09 +0530

    feat(js-basics): pushing if else, else if and basic nested conditions to the flow

---
+++ ifelse.decisionmaking.js
@@ -0,0 +1,22 @@
+//We will be covering about if else decision making statements and also how to optimise decision making using else if and nested if else statements for multiple checks
+//on given outputs to get the desired result.
+
+const userInput = prompt("Enter some random number");
+
+if (userInput <= 10) {
+    alert("Nope this want expected");
+}
+else if (userInput <= 50) {
+    alert("You are close to the expected range");
+}
+else if (userInput >= 50) {
+    if (userInput == 88) {
+        alert("Gotcha! You got that The number was 88!");
+    }
+    else {
+        alert("You are in right range you need to get hold of exact number");
+    }
+}
+else {
+    alert("Nope You are no where near to the expected range or number :(");
+}

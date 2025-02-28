860569107602a2ef085498d49eb794a5a2dd48c5
samarthkulkarni-cb <samarthkulkarni@channelblend.in>
2025-03-01 00:34:01 +0530

    feat(ps-pq's): basic problem to add two numbers

--- PS & PQ's/01.basics.js
+++ PS & PQ's/01.basics.js
@@ -1,1 +1,28 @@
 //Basic With JS - Variable, Operator and Operations With Operator.
+
+//Q1 sum of two integers.
+
+//M1 - declare two variables and console the sum.
+
+const varone = 10;
+const vartwo = 20;
+
+console.log("The sum of varone and vartwo is: ", +(varone + vartwo)); //Addded extra () for BODMAS else concatenation iykyk :)
+
+//M2 - accepting input from user to add a variable and console the sum.
+
+const varthree = Number.parseInt(prompt("Please enter the value for varthree"));
+const varfour = Number.parseInt(prompt("Please enter the value for varfour"));
+const sum = varthree + varfour;
+
+console.log(`The sum of ${varthree} and ${varfour} is ${sum}`);
+
+//M3 - Doing it using a function
+
+const addtwovariables = function (varfive, varsix) {
+  sum = varfive + varsix;
+  return sum;
+};
+
+addtwovariables(10, 20);
+console.log(sum);
